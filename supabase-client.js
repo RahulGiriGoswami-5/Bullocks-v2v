bdb0a0a16eecf8dd9e8cefc7b97baacf8e13db43
Rahul Giri Goswami <rulxxxy@gmail.com>
2026-07-10 16:23:09 +0530

    added supabase

---
+++ supabase-client.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://jishbocwqncgfxeggezq.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_w9NQcYTwbf5xdVMjDZ7F1w_2rnIyKgy";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
